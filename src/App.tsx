676e6d988e72b89786a2cfdacd29640724c1f50f
craig beswetherick <craig@grindheadgames.com>
2018-12-11 16:17:54 +0000

    prettier working intro video working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import './App.css';
 import IntroSequence from './components/intro/intro-sequence';
-import logo from './logo.svg';
 import * as States from './utils/states';
 
 class App extends React.Component {
-
   private content: any;
 
   public componentDidMount() {
@@ -13,31 +11,21 @@ class App extends React.Component {
   }
 
   public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-        {this.content}
-
-      </div>
-    );
+    return <div className="App">{this.content}</div>;
   }
 
   public setAppState(state: number) {
-    switch(state) {
-      case States.STATE_INTRO_SEQUENCE: 
-      this.content = <IntroSequence />;
-      break;
+    switch (state) {
+      case States.STATE_INTRO_SEQUENCE:
+        this.content = <IntroSequence />;
+        break;
 
       default:
-      throw new Error('state is not handled correctly ' + state);
-      break;
+        throw new Error('state is not handled correctly ' + state);
+        break;
     }
 
-    this.setState({state});
+    this.setState({ state });
   }
 }
 
--- src/components/intro/intro-sequence.tsx
+++ src/components/intro/intro-sequence.tsx
@@ -1,13 +1,34 @@
 import * as React from 'react';
+import videojs from 'video.js';
 import './intro-sequence.css';
 
 class IntroSequence extends React.Component {
   public render() {
     return (
-      <div className="Intro-Sequence">
-        Intro Animation here.
-      </div>
+      <video
+        id="intro-sequence"
+        className="video-js"
+        preload="auto"
+        data-setup="{}"
+      >
+        <source src="assets/video/ident.mp4" type="video/mp4" />
+        {/* <source src="MY_VIDEO.webm" type='video/webm' /> */}
+        <p className="vjs-no-js">
+          To view this video please enable JavaScript, and consider upgrading to
+          a web browser that
+          <a href="https://videojs.com/html5-video-support/" target="_blank">
+            supports HTML5 video
+          </a>
+        </p>
+      </video>
     );
+  }
+
+  public componentDidMount() {
+    videojs('intro-sequence', {
+      autoplay: true,
+      fluid: true,
+    });
   }
 }
 
